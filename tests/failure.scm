b91a4c79be91a75d1640a1adfb984e0838f782db
Takeshi Abe <tabe@fixedpoint.jp>
2009-10-22 13:44:42 +0900

    check the returned value

--- tests/failure.scm
+++ tests/failure.scm
@@ -2,18 +2,23 @@
 
 (import (rnrs) (xunit))
 
-(fail! "this is an issue")
-(assert-raise error? (raise (make-message-condition "OK")))
-(assert-raise integer? (+ 1 1))
-(assert-= 1 (+ 1 1))
-(assert-boolean=? #t (not (not #f)))
-(assert-char-ci=? #\Z (integer->char (+ 25 (char->integer #\Ａ))))
-(assert-char=? #\Z (integer->char (+ 25 (char->integer #\a))))
-(assert-string=? "R6RS" (string-append "R" (number->string 5) "RS"))
-(assert-boolean? 't)
-(assert-zero? (+ 1 1))
+(define-syntax should-be-false
+  (syntax-rules ()
+    ((_ expr)
+     (assert (not expr)))))
+
+(should-be-false (fail! "this is an issue"))
+(should-be-false (assert-raise error? (raise (make-message-condition "OK"))))
+(should-be-false (assert-raise integer? (+ 1 1)))
+(should-be-false (assert-= 1 (+ 1 1)))
+(should-be-false (assert-boolean=? #t (not (not #f))))
+(should-be-false (assert-char-ci=? #\Z (integer->char (+ 25 (char->integer #\Ａ)))))
+(should-be-false (assert-char=? #\Z (integer->char (+ 25 (char->integer #\a)))))
+(should-be-false (assert-string=? "R6RS" (string-append "R" (number->string 5) "RS")))
+(should-be-false (assert-boolean? 't))
+(should-be-false (assert-zero? (+ 1 1)))
 (skip-unless #t
-  (assert-boolean=? #t #f)
-  (assert-= 0 1))
+  (should-be-false (assert-boolean=? #t #f))
+  (should-be-false (assert-= 0 1)))
 
 (report)
